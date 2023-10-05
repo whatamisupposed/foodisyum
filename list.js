90753cfd5f71694eefa0310c6ce2cc6db7a1f771
Emily <twymanem@gmail.com>
2023-10-04 23:58:52 -0600

    fse

---
+++ list.js
@@ -0,0 +1,91 @@
+const clickMeDiv = document.getElementById('clickMe');
+const contentContainer = document.getElementById('contentContainer');
+
+clickMeDiv.addEventListener('click', function() {
+  contentContainer.classList.toggle('darkMode');
+});
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+
+  ];
+
+  const appendToMeDiv = document.getElementById('appendToMe');
+
+storeItems.forEach(item => {
+
+  const paragraph = document.createElement('p');
+
+  if (item.inStock) {
+    paragraph.textContent = `Name: ${item.name}, Price: $${item.price}`;
+    paragraph.classList.add('inStock');
+  } else {
+    paragraph.textContent = `Name: ${item.name}, Price: $${item.price} Not in stock`;
+    paragraph.classList.add('notInStock');
+  }
+  appendToMeDiv.appendChild(paragraph);
+});
